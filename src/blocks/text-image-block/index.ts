f69edb2d5669613175941414c16b65873c5ecaf9
silvagnidario-code <silvagni.dario@gmail.com>
2026-08-20 21:37:29 +0200

    Delete src/blocks/text-image-block directory

--- src/blocks/text-image-block/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Salva come: src/blocks/text-image-block/index.ts
- * Permette a RenderBlocks.tsx di fare: import { TextImageBlock } from './text-image-block'
- */
-export { TextImageBlock } from './Component'
-export { TextImage } from './config'
