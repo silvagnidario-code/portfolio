e68a26708ae8c3d811940c2f9872b7cb9598a721
silvagnidario-code <silvagni.dario@gmail.com>
2026-08-24 18:03:54 +0200

    Add new migration for gallery variant

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -2,6 +2,7 @@ import * as migration_20260815_155850_initial from './20260815_155850_initial';
 import * as migration_20260815_181959_cms_schema from './20260815_181959_cms_schema';
 import * as migration_20260816_220811 from './20260816_220811';
 import * as migration_20260819_171003 from './20260819_171003';
+import * as migration_20260824_170500_add_gallery_variant from './20260824_170500_add_gallery_variant';
 
 export const migrations = [
   {
